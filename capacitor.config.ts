458a7ad02b120f6112f13aeaeac760596d680e08
LeonelAcostaCruz <acostaleo18@gmail.com>
2024-11-04 19:08:20 -0700

    inicio de Tools

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,11 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Tools',
+  appId: 'app.web.Elx.Tools',
+  appName: 'ToolsElxApp',
   webDir: 'www'
 };
 
 export default config;
+
+//Elx.Tools.web.app
